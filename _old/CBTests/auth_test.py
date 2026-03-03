033ca9d4eccbd5d43eaaf850c5202a8e05a1d7e4
ACertainArchangel <165073349+ACertainArchangel@users.noreply.github.com>
2026-03-03 15:21:36 -0800

    Clarify example in auth_test.py

--- _old/CBTests/auth_test.py
+++ _old/CBTests/auth_test.py
@@ -1,7 +1,7 @@
 from coinbase import jwt_generator
 import requests
 
-api_key_name = "organizations/12ca2c06-851f-4dc2-a60a-3da41a85b53e/apiKeys/2fc439d7-c773-446b-90b3-270009067642"
+api_key_name = "organizations/12ca2c06-851f-4dc2-a60a-3da41a85b53e/apiKeys/2fc439d7-c773-446b-90b3-270009067642" # Example key, not real
 api_private_key = "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIGmJ+xqMhFcRn3KeCbzFrDbw++ChQJTN9qbkDTLOvgwBoAoGCCqGSM49\nAwEHoUQDQgAEzuRvBUMFfcCes98XXT07I+86DyOsLCIB1wl99/B09Oq5PHUc0HWC\n8lbpjSTUje2F/PbdVA8E8WIBRasUqCrDLQ==\n-----END EC PRIVATE KEY-----\n"
 
 method = "GET"
